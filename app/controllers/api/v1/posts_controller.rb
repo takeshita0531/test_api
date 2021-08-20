96eadf04b4826c595948c954f7af35f747140dc8
takeshita0531 <matubara0531@ezweb.ne.jp>
2021-08-20 01:20:29 +0000

    Add post_controller api create

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -4,8 +4,23 @@ module Api
       before_action :set_post, only: [:show, :update, :destroy]
 
       def index
-        posts = "test"
+        posts = Post.all
         render json: { status: 'SUCCESS', message: 'Loaded posts', data: posts }
+      end
+      
+      def create
+          post_new = Post.new(post_params)
+          if post_new.save
+              render json: { status: 'SUCCESS', data: post_new }
+          else
+              render json: { status: 'SUCCESS', data: post_new.error }
+          end
+      end
+      
+      private
+      
+      def post_params
+          params.require(:post).permit(:title)
       end
     end
   end
