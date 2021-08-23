846c19860236193169c675c7736e0309850cd6a3
takeshita0531 <matubara0531@ezweb.ne.jp>
2021-08-23 06:47:00 +0000

    deploy

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -37,7 +37,7 @@ module Api
       private
       
         def authenticate
-          authenticate_or_request_with_http_token do |token,options|
+          authenticate_or_request_with_http_token do |token, options|
             auth_user = User.find_by(token: token)
             auth_user != nil ? true : false
           end
